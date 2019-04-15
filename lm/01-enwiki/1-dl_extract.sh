e579e6fdcf8525063e89cfce5b85cb8cb108e5f0
Ryan Hileman <lunixbochs@gmail.com>
2019-04-15 10:49:45 -0700

    add lm/ and enwiki download/extractor

---
+++ lm/01-enwiki/1-dl_extract.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -u
+aria2c https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles-multistream.xml.bz2
+git clone https://github.com/attardi/wikiextractor.git
+python3 wikiextractor/WikiExtractor.py -b100M -c --no_templates --filter_disambig_pages enwiki-latest-pages-articles-multistream.xml.bz2
